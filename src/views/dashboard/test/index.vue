d9b3de0c314ca3d4079b997c20e4c9effb4f43fc
Leon <qinlanfeng@deepexi.com>
2021-08-24 11:02:56 +0800

    引入sass

--- src/views/dashboard/test/index.vue
+++ src/views/dashboard/test/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>dashboard test</div>
+  <div class="dashboard-wrap">
+    <h3>test dashboard</h3>
+    <p class="mt-3">
+      sdfsa
+    </p>
+    <div class="p-4">
+      alsdkfjalsdfa
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.dashboard-wrap {
+  h3 {
+    background-color: yellow;
+  }
+
+  p {
+    background-color: red;
+  }
+
+  div {
+    background-color: green;
+  }
+}
+</style>
