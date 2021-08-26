013b9ee4f629b8b179627997ac4eaa416f0ef7d1
Leon <qinlanfeng@deepexi.com>
2021-08-26 15:03:23 +0800

    fix: 修改e2e测试用例

--- tests/e2e/specs/index.test.ts
+++ tests/e2e/specs/index.test.ts
@@ -1,6 +1,6 @@
 describe('Vue 3 Playground', () => {
   it('should display playground text on page', () => {
     cy.visit('/')
-    cy.contains('h2', 'test dashboard')
+    cy.contains('h3', 'test dashboard')
   })
 })
